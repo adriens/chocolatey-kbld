83af2ef58c22a5b2cbdbf9ee8443a3d23ff81de6
adriens <Adrien.Sales@gmail.com>
2020-12-31 10:04:00 +1100

    Applyting best chocolatey best practices

---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$bat = Join-Path $content 'kbld'
+
+Uninstall-BinFile `
+  -Name 'kbld' `
+  -Path $bat
